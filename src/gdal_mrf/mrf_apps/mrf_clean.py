08ddc93dbea5ccfd8c3f2bc3adf5f9976b0fa54b
Lucian Plesea <lucianpls@gmail.com>
2018-11-09 16:41:11 -0800

    Update mrf_clean, slightly more efficient
    Should be faster over empty areas, fewer system calls

--- src/gdal_mrf/mrf_apps/mrf_clean.py
+++ src/gdal_mrf/mrf_apps/mrf_clean.py
@@ -7,8 +7,10 @@
 #
 # Created:     05/10/2016
 # Updated:     07/07/2017 Creates index files with holes if possible
+# Updated:     11/09/2018 Use typed arrays instead of struct
+#                         Process index file block at a time
 #
-# Copyright:   (c) lucian 2016 - 2017
+# Copyright:   (c) lucian 2016 - 2018
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,54 +26,58 @@
 #
 #-------------------------------------------------------------------------------
 
-import struct
 import os
 import os.path
 import sys
-
-def index_name(mrf_name):
-    bname, ext = os.path.splitext(mrf_name)
-    return bname + os.extsep + "idx"
-
-def write_idx(f, l):
-    'Write a chunk of the index from list l to file f'
-    if sum(i[1] for i in l):
-        f.write(struct.pack(">{0}Q".format(2 * len(l)), *sum(l, [])))
-    else:
-        f.seek(16 * len(l), os.SEEK_CUR)
+import array
 
 # empty_file content is used to initialize the data file
-def main(source, destination, empty_file = None):
+def mrf_clean(source, destination, empty_file = None):
     '''Copies the active tile from a source to a destination MRF'''
-    sidx = open(index_name(source), "rb")
-    sfile = open(source, "rb")
-    # Create the output files
-    didx = open(index_name(destination),"wb")
-    dfile = open(destination, "wb")
-    if empty_file:
-        dfile.write(open(empty_file,"rb").read())
-    doffset = dfile.tell()
 
-    lidx = [] # buffer index records in a list, to avoid writing zero blocks
-    # Copy every tile in sequence
-    for tile in iter(lambda: sidx.read(16), ''):
-        offset, size = struct.unpack(">2Q", tile)
-        if size:
-            sfile.seek(offset, os.SEEK_SET)
-            dfile.write(sfile.read(size))
-            offset = doffset
-            doffset += size
-        else:
-            offset = 0
-        lidx.append((offset, size))
-        if 32 == len(lidx): # 32 tile records, 512 bytes
-            write_idx(didx, lidx)
-            lidx = []
+    def index_name(mrf_name):
+        bname, ext = os.path.splitext(mrf_name)
+        return bname + os.extsep + "idx"
 
-    # Write the remaining index chunk and mark the file end
-    if len(lidx):
-        write_idx(didx, lidx)
-    didx.truncate()
+    with open(index_name(source), "rb") as sidx:
+        with open(source, "rb") as sfile:
+            with open(index_name(destination),"wb") as didx:
+                with open(destination, "wb") as dfile:
+                    if empty_file:
+                        dfile.write(open(empty_file,"rb").read())
+                    doffset = dfile.tell()
+
+                    while True:
+                        idx = array.array('Q')
+                        try:
+                            idx.fromfile(sidx, 512 // idx.itemsize)
+                        except: # Could be incomplete last block or EOF
+                            if len(idx) == 0:
+                                break # Normal exit
+
+                        # Don't write empty blocks
+                        if idx.count(0) == len(idx):
+                            didx.seek(len(idx) * idx.itemsize, os.SEEK_CUR)
+                            continue
+
+                        if sys.byteorder != 'big':
+                            idx.byteswap() # To native
+
+                        # copy tiles in this block, adjust the offsets
+                        for i in range(0, len(idx), 2):
+                            if idx[i + 1] != 0: # size of tile
+                                sfile.seek(idx[i], os.SEEK_SET)
+                                idx[i] = doffset
+                                doffset += idx[i + 1]
+                                dfile.write(sfile.read(idx[i+1]))
+
+                        if sys.byteorder != 'big':
+                            idx.byteswap() # Back to big before writing
+
+                        idx.tofile(didx)
+
+                    didx.truncate() # In case last block is empty
+
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    mrf_clean(*sys.argv[1:])
